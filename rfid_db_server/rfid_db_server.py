0ce38675d597409074d9c1737307cf7bea02fbc4
Maciej Walaszczyk <walaszczykm@gmail.com>
2016-10-20 11:00:42 +0200

    init repository with basic code for arduino and python server

---
+++ rfid_db_server/rfid_db_server.py
@@ -0,0 +1,37 @@
+from socket import *
+from threading import *
+import sys
+
+#CONFIG
+port = 5555
+host = ''
+s = socket(AF_INET, SOCK_STREAM)
+
+def client_thred(conn, ip):
+   print(ip + ' CONNECT')
+   while True:
+       data = conn.recv(4096)
+       if not data:
+        print(ip + " DISCONNECT")
+        break
+       data = data.decode('utf-8')
+       print(ip + ' SENT "' + data + '"')
+       conn.send(str('OK').encode())
+       
+
+def server_loop():
+    while True:
+        conn, addr = s.accept()
+        Thread(target=client_thred, args=(conn, addr[0]), daemon=True).start()
+
+def main():
+    try:
+        s.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+    s.listen()
+    server_loop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ rfid_eth_client/rfid_eth_client.ino
@@ -0,0 +1,43 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte server[] = {10, 0, 0, 101};
+EthernetClient client;
+
+void snedToServer(String msg)
+{
+  if(client.connect(server, 5555))
+  {
+    client.print(msg);
+    client.flush();
+    while(client.available() < 2) {}
+    char c;
+    while(true)
+    {
+      c = client.read();
+      if(c == -1)
+      {
+        break;
+      }
+      Serial.print(c);
+    }
+    Serial.println("");
+    client.stop();
+    Serial.println("MSG SENT");
+  }
+}
+
+void setup() {
+  Ethernet.begin(mac);
+  Serial.begin(9600);
+  while(!Serial){}
+
+  Serial.print("IP: ");
+  Serial.println(Ethernet.localIP());
+}
+
+void loop() {
+  snedToServer("hello");
+  delay(2000);
+}
